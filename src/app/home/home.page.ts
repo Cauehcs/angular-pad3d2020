4ed4e209b86ad9321356863270400b4fd7ff67df
Cauehcs <caue.hcs@gmail.com>
2020-07-08 08:11:24 -0300

    Caminhando entre páginas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
